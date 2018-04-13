b4a7cb151c4a5f5b31ec369611b8b38cb53893dc
jianan shang <18203355025@163.com>
2018-04-13 11:10:30 +0800

    将Tab6的代码全部放到一个文件里

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -127,12 +127,7 @@ namespace WpfApplication1
             Init_Tab2_ComboBox();
         }
 
-        //地图维护
-        private void Tab6_Back_Button_Click(object sender, RoutedEventArgs e)
-        {
-            tabcontrol.SelectedIndex = 1;
-            Init_Tab2_ComboBox();
-        }
+        
 
         //其他
         private void Tab7_Back_Button_Click(object sender, RoutedEventArgs e)
@@ -141,64 +136,7 @@ namespace WpfApplication1
             Init_Tab2_ComboBox();
         }
 
-        
-
-        #region//地图功能的实现
-        private void img_MouseDown_Tab6(object sender, MouseButtonEventArgs e)
-        {
-            isMouseLeftButtonDown_Tab6 = true;
-            previousMousePoint_Tab6 = e.GetPosition(img_Tab6);
-        }
-
-        private void img_MouseUp_Tab6(object sender, MouseButtonEventArgs e)
-        {
-            isMouseLeftButtonDown_Tab6 = false;
-        }
-
-        private void img_MouseLeave_Tab6(object sender, MouseEventArgs e)
-        {
-            isMouseLeftButtonDown_Tab6 = false;
-        }
-
-        private void img_MouseMove_Tab6(object sender, MouseEventArgs e)
-        {
-            if (isMouseLeftButtonDown_Tab6 == true)
-            {
-                Point position_Tab6 = e.GetPosition(img_Tab6);
-                tlt_Tab6.X += (position_Tab6.X - this.previousMousePoint_Tab6.X) * sfr_Tab6.ScaleX;
-                tlt_Tab6.Y += (position_Tab6.Y - this.previousMousePoint_Tab6.Y) * sfr_Tab6.ScaleY;
-
-                tlt2_Tab6.X += (position_Tab6.X - this.previousMousePoint_Tab6.X) * sfr_Tab6.ScaleX;
-                tlt2_Tab6.Y += (position_Tab6.Y - this.previousMousePoint_Tab6.Y) * sfr_Tab6.ScaleY;
-
-            }
-        }
-
-        private void img_MouseWheel_Tab6(object sender, MouseWheelEventArgs e)
-        {
-            if (sfr_Tab6.ScaleX < 0.2 && sfr_Tab6.ScaleY < 0.2 && e.Delta < 0)
-            {
-                return;
-            }
-            Point centerPoint_Tab6 = e.GetPosition(img_Tab6);
-            Point pt_Tab6 = img_Tab6.RenderTransform.Inverse.Transform(centerPoint_Tab6);
-            this.tlt_Tab6.X = (centerPoint_Tab6.X - pt_Tab6.X) * this.sfr_Tab6.ScaleX;
-            this.tlt_Tab6.Y = (centerPoint_Tab6.Y - pt_Tab6.Y) * this.sfr_Tab6.ScaleY;
-            this.sfr_Tab6.CenterX = centerPoint_Tab6.X;
-            this.sfr_Tab6.CenterY = centerPoint_Tab6.Y;
-            this.sfr_Tab6.ScaleX += e.Delta / 1000.0;
-            this.sfr_Tab6.ScaleY += e.Delta / 1000.0;
-
-            Point centerPoint2_Tab6 = e.GetPosition(rectangle1_Tab6);
-            Point pt2_Tab6 = rectangle1_Tab6.RenderTransform.Inverse.Transform(centerPoint2_Tab6);
-            this.tlt2_Tab6.X = (centerPoint2_Tab6.X - pt2_Tab6.X) * this.sfr2_Tab6.ScaleX;
-            this.tlt2_Tab6.Y = (centerPoint2_Tab6.Y - pt2_Tab6.Y) * this.sfr2_Tab6.ScaleY;
-            this.sfr2_Tab6.CenterX = centerPoint2_Tab6.X;
-            this.sfr2_Tab6.CenterY = centerPoint2_Tab6.Y;
-            this.sfr2_Tab6.ScaleX += e.Delta / 1000.0;
-            this.sfr2_Tab6.ScaleY += e.Delta / 1000.0;
-        }
-        #endregion
+                
     }
 
     public class test5_mem : INotifyPropertyChanged
