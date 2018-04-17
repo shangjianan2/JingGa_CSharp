2221bd017154dad8cc0345fbb3fc206f11516724
jianan shang <18203355025@163.com>
2018-04-17 14:18:38 +0800

    成功实现"添加用户"的功能 在"删除用户"的功能中测试了Tab4_User_ListView.SelectedIndex这个变量的功能，发现通过这个变量可以获取到当前选中选项的序号

--- WpfApplication1/Tab4.cs
+++ WpfApplication1/Tab4.cs
@@ -46,6 +46,63 @@ namespace WpfApplication1
             }
         }
 
+        private void Tab4_AddUser_Button_Click(object sender, EventArgs e)
+        {
+            if (Tab4_AddUserName_TextBox.Text == "" || Tab4_AddPassWord_TextBox.Text == "")
+                return;
+
+            //insert into users (`name`, `password`) VALUES ( );
+            string temp_str = "insert into users (`name`, `password`) VALUES ( \"" + Tab4_AddUserName_TextBox.Text + "\", \"" + Tab4_AddPassWord_TextBox.Text + "\" );";
+            try
+            {
+                MySqlHelper.GetDataSet(MySqlHelper.Conn, CommandType.Text, temp_str, null);
+            }
+            catch
+            {
+                MessageBox.Show("此用户已添加", "error");
+            }
+            Tab4_User_ListView.Items.Refresh();//添加这个就好使
+            //为Tab4（用户维护界面）中的listview初始化
+            //Init_Tab4_CurrentStatus_ListView(ref test5_Mem_Tab4_array, Tab4_User_ListView);
+            Init_test5_Mem_Tab4_array(ref test5_Mem_Tab4_array);
+            Init_Tab1_ComboBox();
+        }
+
+        private void Tab4_DeleteUser_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //int temp_index = Tab4_User_ListView.SelectedItems[0].Index;
+                //string DeletedUser = ExistYongHu_YongHuWeiHu_ListView.Items[temp_index].SubItems[0].Text;
+
+                //if (DeletedUser == "root")//不能删除root用户
+                //    return;
+
+                //MessageBoxButtons messButton = MessageBoxButtons.OKCancel;
+                //DialogResult dr = MessageBox.Show("确定要删除？", "用户删除", messButton);
+                //if (dr == DialogResult.OK)
+                //{
+                //    string command_str = "delete from users where name=\"" + DeletedUser + "\"";
+                //    MySqlHelper.GetDataSet(MySqlHelper.Conn, CommandType.Text, command_str, null);
+                //}
+                //else
+                //{
+                //    return;//既然没有做出改变，就不用进行Update_ExistYongHu_YongHuWeiHu_ListView和Init_Login_ComboBox
+                //}
+
+                System.Diagnostics.Debug.WriteLine("{0}", Tab4_User_ListView.SelectedIndex);
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("请选择一个用户", "error");
+            }
+            Init_test5_Mem_Tab4_array(ref test5_Mem_Tab4_array);
+            Init_Tab1_ComboBox();
+        }
+
         //Tab用户维护
         private void Tab4_Back_Button_Click(object sender, RoutedEventArgs e)
         {
